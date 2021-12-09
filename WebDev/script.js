ebe8340c2024be950eec976e078478313da37ea6
systamii <87766262+systamii@users.noreply.github.com>
2021-12-09 18:25:37 -0500

    Delete WebDev directory

--- WebDev/script.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("nav_div");
-    if (x.className === "nav_div") {
-      x.className += " responsive";
-    } else {
-      x.className = "nav_div";
-    }
-  }
